46597ff019b34ca576b68c2c48604e695a3a8d70
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-09-29 17:38:32 +0300

    Add files via upload

---
+++ lab4_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+	int d;
+	float pi, L;
+
+	pi = 3.14;
+
+	printf("Enter d: ");
+	scanf_s("%d", &d);
+
+	L = pi * d;
+	printf("L is %f\n", L);
+
+	return 0;
+}
