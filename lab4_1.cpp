44a64d98d2aed098742166bfc8c4cc29e1c50e57
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-09-29 17:24:32 +0300

    Add files via upload

---
+++ lab4_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+	int a, b;
+
+	printf("Enter a: ");
+	scanf_s("%d", &a);
+
+	printf("Enter b: ");
+	scanf_s("%d", &b);
+
+	int S, P;
+
+	S = a * b;
+	P = 2 * (a + b);
+
+	printf("S is %d\n", S);
+	printf("P is %d\n", P);
+
+	return 0;
+}
