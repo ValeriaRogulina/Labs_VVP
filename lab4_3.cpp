c2edaca01991224ae31f3495364f38ca88db5365
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-09-29 17:41:07 +0300

    Add files via upload

---
+++ lab4_3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+	int a, b;
+	float Sr;
+
+	printf("Enter a: ");
+	scanf_s("%d", &a);
+
+	printf("Enter b: ");
+	scanf_s("%d", &b);
+
+	Sr = (a + b) / 2;
+	printf("Sr is %.2f\n", Sr);
+
+	return 0;
+}
