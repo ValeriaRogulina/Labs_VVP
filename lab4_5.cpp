ee1738e9bb3d1ff57e5533475450f7e218556ffd
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-09-29 17:46:26 +0300

    Add files via upload

---
+++ lab4_5.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+	float x, y, x1, y1, sum, mul, R, C;
+
+	printf("Enter x: ");
+	scanf_s("%f", &x);
+
+	printf("Enter y: ");
+	scanf_s("%f", &y);
+
+	x1 = abs(x);
+	y1 = abs(y);
+
+	sum = x1 + y1;
+	mul = x1 * y1;
+	R = x1 > y1 ? x1 - y1 : y1 - x1;
+	C = x1 > y1 ? x1 / y1 : y1 / x1;
+
+	printf("sum is %.2f\n", sum);
+	printf("mul is %.2f\n", mul);
+	printf("R is %.2f\n", R);
+	printf("C is %.2f\n", C);
+
+	return 0;
+}
