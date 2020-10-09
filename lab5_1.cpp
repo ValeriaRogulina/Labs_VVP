02e22b4eaded864077f707d52c6787d443124898
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-10 01:33:35 +0300

    Add files via upload

---
+++ lab5_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x1, x2, y1, y2;
+
+	printf("x1 = ");
+	scanf_s("%d", &x1); //Ввод координаты x1 
+	printf("y1 = ");
+	scanf_s("%d", &y1); //Ввод координаты y1
+
+	printf("x2 = ");
+	scanf_s("%d", &x2); //Ввод координаты x2
+	printf("y2 = ");
+	scanf_s("%d", &y2); //Ввод координаты y2
+
+	float R;
+	int x3 = x2 - x1; //Вычисление координаты x3 вектора
+	int y3 = y2 - y1; //Вычисление координаты y3 вектора
+
+	R = sqrt(pow(x3, 2) + pow(y3, 2)); //Вычисление расстояние между точками
+
+	printf("R is %.3f\n", R); //Вывод результата
+
+	return 0;
+}
