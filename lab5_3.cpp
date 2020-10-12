43cc780e5c7aa90bd4179403c34d00f59d1c5e65
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-12 13:00:59 +0300

    Add files via upload

---
+++ lab5_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, b, c;
+
+	printf("a = ");
+	scanf_s("%d", &a); //Ввод координаты точки A
+
+	printf("b = ");
+	scanf_s("%d", &b); //Ввод координаты точки B
+
+	printf("c = ");
+	scanf_s("%d", &c); //Ввод координаты точки C
+
+	int AC, BC, mul;
+
+	AC = abs(c - a); //Вычисление длины отрезка AC
+	BC = abs(c - b); //Вычисление длины отрезка BC
+	mul = AC * BC;
+
+	printf("AC is %d\n", AC);
+	printf("BC is %d\n", BC);
+	printf("Multiplication is %d\n", mul);
+
+	return 0;
+}
