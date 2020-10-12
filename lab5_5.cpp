35286797b1b34f315cc544f5aa09b99761ea936b
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-12 15:39:15 +0300

    Add files via upload

---
+++ lab5_5.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float x1, y1, x2, y2, x3, y3; 
+
+	printf("x1 = ");
+	scanf_s("%f", &x1);
+	printf("y1 = ");
+	scanf_s("%f", &y1);
+
+	printf("x2 = ");
+	scanf_s("%f", &x2);
+	printf("y2 = ");
+	scanf_s("%f", &y2);
+
+	printf("x3 = ");
+	scanf_s("%f", &x3);
+	printf("y3 = ");
+	scanf_s("%f", &y3);
+
+	float a, b, c, p, P, S;
+	a = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+	b = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2));
+	c = sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2));
+
+	P = a + b + c;
+	p = P / 2;
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
+
+	printf("P is %.3f\n", P);
+	printf("S is %.3f\n", S);
+
+	return 0;
+}
