e7b0fa9ed853772f5f305d0bc532511f68062c9f
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-12 12:29:15 +0300

    Add files via upload

---
+++ lab5_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, b, c;
+
+	printf("a = ");
+	scanf_s("%d", &a);
+
+	printf("b = ");
+	scanf_s("%d", &b);
+
+	printf("c = ");
+	scanf_s("%d", &c);
+
+	int AC, BC, sum;
+
+	AC = abs(c - a);
+	BC = abs(c - b);
+	sum = AC + BC;
+
+	printf("AC is %d\n", AC);
+	printf("BC is %d\n", BC);
+	printf("Sum is %d\n", sum);
+
+	return 0;
+}
