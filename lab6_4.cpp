2c1224a9b96463f8dc70430be8052ee38f6686e2
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-18 20:48:04 +0300

    Add files via upload

---
+++ lab6_4.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float x, y;
+
+	printf("x = ");
+	scanf_s("%f", &x);
+
+	y = 3 * pow(x, 6) - 6 * pow(x, 2) - 7;
+
+	printf("Y = %.3f\n", y);
+	
+	return 0;
+}
---
+++ lab6_5.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float x, y;
+
+	printf("x = ");
+	scanf_s("%f", &x);
+
+	y = 4 * pow((x-3), 6) - 7 * pow((x-3), 3) + 2;
+
+	printf("Y = %.3f\n", y);
+
+	return 0;
+}
