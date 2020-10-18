97bb9434478f20774965e76e60a4d3735b460805
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-18 20:33:55 +0300

    Add files via upload

---
+++ lab6_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main () 
+{
+int A, B, C;
+
+printf("A = ");
+scanf_s("%d", &A);
+
+printf("B = ");
+scanf_s("%d", &B);
+
+C = A;
+A = B;
+B = C;
+
+printf("A = %d\n", A);
+printf("B = %d\n", B);
+
+return 0;
+}
---
+++ lab6_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int A, B, C, D;
+
+	printf("A = ");
+	scanf_s("%d", &A);
+
+	printf("B = ");
+	scanf_s("%d", &B);
+
+	printf("C = ");
+	scanf_s("%d", &C);
+
+	D = A;
+	A = B;
+	B = C;
+	C = D;
+
+	printf("A = %d\nB = %d\nC = %d\n", A, B, C);
+	return 0;
+}
