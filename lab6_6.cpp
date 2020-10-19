9f227bab9b775a5e18aa75a17d8a8cdbc4a13094
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-19 12:43:12 +0300

    Add files via upload

---
+++ lab6_6.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int A, A1;
+
+	printf("A = ");
+	scanf_s("%d", &A);
+
+	A1 = A * A; //Возведение А в квадрат
+	A1 = A1 * A1; //Возведение А в 4 степень
+	A1 = A1 * A1; //Возведение А в 8 степень
+
+	printf("A^8 = %d\n", A1);
+
+	return 0;
+}
---
+++ lab6_7.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int A, A1, A2;
+
+	printf("A = ");
+	scanf_s("%d", &A);
+
+	A1 = A * A; //Возведение А в квадрат
+	A2 = A1 * A; //Возведение А в куб
+	A2 = A2 * A1; //Возведение А в 5 степень
+	A2 = A2 * A2 * A2; //Возведение А в 15 степень
+
+	printf("A^15 = %d\n", A2);
+
+	return 0;
+}
