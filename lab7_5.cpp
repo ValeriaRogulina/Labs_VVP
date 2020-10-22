5e010c508cdb02837c97b428ddcb5e826c342754
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-22 22:13:06 +0300

    Add files via upload

---
+++ lab7_5.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float A, B, x;
+
+	printf("A = ");
+	scanf_s("%f", &A);
+
+	printf("B = ");
+	scanf_s("%f", &B);
+
+	x = -B / A;
+
+	printf("x = %.3f\n", x);
+
+	return 0;
+}
