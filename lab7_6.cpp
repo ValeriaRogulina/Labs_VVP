42d36f575ba9774278941e06427e6c05ce99c717
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-22 22:37:15 +0300

    Add files via upload

---
+++ lab7_6.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int A1, B1, C1, A2, B2, C2, x, y, det;
+
+	printf("A1 = ");
+	scanf_s("%d", &A1);
+
+	printf("B1 = ");
+	scanf_s("%d", &B1);
+
+	printf("C1 = ");
+	scanf_s("%d", &C1);
+
+	printf("A2 = ");
+	scanf_s("%d", &A2);
+
+	printf("B2 = ");
+	scanf_s("%d", &B2);
+
+	printf("C2 = ");
+	scanf_s("%d", &C2);
+
+	det = (A1 * B2) - (B1 * A2);
+	
+
+	if (det == 0) {
+		printf("Result is undefined");
+	}
+	else if (det !=0) {
+		x = ((C1 * B2) - (B1 * C2)) / det;
+		y = ((A1 * C2) - (C1 * A2)) / det;
+	}
+	
+	printf("x = %d\n", x);
+	printf("y = %d\n", y);
+
+	return 0;
+}
