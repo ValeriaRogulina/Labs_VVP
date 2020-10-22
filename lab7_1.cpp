583f7a2842089da61d3de0e5162c52900fdce65e
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-22 21:56:12 +0300

    Add files via upload

---
+++ lab7_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, pi = 3.14;
+
+	printf("a = ");
+	scanf_s("%f", &a);
+
+	a = a * pi / 180; //Перевод в радианы
+
+	printf("a = %f\n", a);
+
+	return 0;
+}
---
+++ lab7_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, pi = 3.14;
+
+	printf("a = ");
+	scanf_s("%f", &a);
+
+	a = 180 * a / pi; //Перевод в градусы
+
+	printf("a = %f\n", a);
+
+	return 0;
+}
