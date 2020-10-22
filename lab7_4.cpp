0292c13d0175caab4ac494ce00f4780c62567d63
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-22 22:10:07 +0300

    Add files via upload

---
+++ lab7_4.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float V1, V2, T, S;
+
+	printf("V1 = ");
+	scanf_s("%f", &V1);
+
+	printf("V2 = ");
+	scanf_s("%f", &V2);
+
+	printf("S = ");
+	scanf_s("%f", &S);
+
+	printf("T = ");
+	scanf_s("%f", &T);
+
+
+	S = S + V1 * T + V2 * T;
+
+	printf("S = %.3f\n", S);
+
+	return 0;
+}
