79c589c9bcd06ed32bbf9595a1ccd7a751b91d25
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-22 22:03:10 +0300

    Add files via upload

---
+++ lab7_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float X, Y, kg1, A, S;
+
+	printf("X = ");
+	scanf_s("%f", &X);
+
+	printf("Y = ");
+	scanf_s("%f", &Y);
+
+	printf("A = ");
+	scanf_s("%f", &A);
+
+
+	kg1 = A / X; //Цена 1 кг
+	S = kg1 * Y; //Стоимость Y конфет 
+
+	printf("kg1 = %.3f\n", kg1);
+	printf("S = %.3f\n", S);
+
+	return 0;
+}
