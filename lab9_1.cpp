29777906ba68414d95459603256ba2d0a8d20f5d
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-27 22:23:34 +0300

    Add files via upload

---
+++ lab9_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int N, S;
+
+    printf("N = ");
+    scanf_s("%d", &N);
+
+    S = N % 60; 
+
+    printf("S = %d\n", S);
+
+    return 0;
+}
---
+++ lab9_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int K;
+
+    printf("1<=K<=365, K = ");
+    scanf_s("%d", &K);
+
+    K = K % 7; //Делим К на кол-во дней в неделе, тк год начался с понедельника
+
+    printf("K = %d\n", K);
+
+    return 0;
+}
