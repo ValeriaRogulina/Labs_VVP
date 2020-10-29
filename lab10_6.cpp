01cc7f4c520100ab439a3614c77b149ab87139ac
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-29 21:03:46 +0300

    Add files via upload

---
+++ lab10_6.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+
+    printf("a = ");
+    scanf_s("%d", &a);
+
+    printf("b = ");
+    scanf_s("%d", &b);
+
+    printf("c = ");
+    scanf_s("%d", &c);
+
+    if ((c*c == a*a + b*b) | (a*a == b*b + c*c) | (b*b == a*a + c*c)) {
+        printf("Треугольник прямоугольный");
+    }
+    else {
+        printf("Треугольник не прямоугольный");
+    }
+
+    return 0;
+}
