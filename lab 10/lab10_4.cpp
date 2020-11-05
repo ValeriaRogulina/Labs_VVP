9e9f41bd2927de213074a0d245c5a8d06a5dea59
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-11-05 13:44:59 +0300

    Add files via upload

---
+++ lab 10/lab10_4.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int A, a1, a2, a3;
+
+    printf("A = ");
+    scanf_s("%d", &A);
+
+    a1 = A / 100;
+    a2 = (A / 10) % 10;
+    a3 = A % 10;
+
+    if (((a1 < a2) & (a2 < a3)) | ((a1 > a2) & (a2 > a3))) {
+        printf("Цифры данного числа образуют возрастающую или убывающую последовательность");
+    }
+    else {
+        printf("Цифры числа не образуют последовательность");
+    }
+
+    return 0;
+}
---
+++ lab 10/lab10_5.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int A, a1, a2, a3, a4;
+
+    printf("A = ");
+    scanf_s("%d", &A);
+
+    a1 = A / 1000;
+    a2 = (A % 1000) / 100;
+    a3 = (A % 100) / 10;
+    a4 = A % 10;
+
+    if ((a1 == a4) & (a2 == a3)) {
+        printf("Число является палиндромом");
+    }
+    else {
+        printf("Число не является палиндромом");
+    }
+
+    return 0;
+}
