93072bf96ce551517f966cccbdd0c302b9ee5080
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-11-09 20:17:19 +0300

    Add files via upload

---
+++ lab 12/lab12_2.cpp
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int O;
+    char C;
+    printf("Enter course: ");
+    scanf_s("%c", &C);
+    printf("Operation: ");
+    scanf_s("%d", &O);
+
+    switch (C) {
+    case 'N':
+        switch (O) {
+        case 1:
+            printf("W"); //West (запад)
+            break;
+        case 0:
+            printf("N"); //North (север)
+            break;
+        case -1:
+            printf("E"); //East (восток)
+            break;
+        }
+        break;
+    case 'E':
+        switch (O) {
+        case 1:
+            printf("N");
+            break;
+        case 0:
+            printf("E");
+            break;
+        case -1:
+            printf("S"); //South (юг)
+            break;
+        }
+    case 'S':
+        switch (O) {
+        case 1:
+            printf("E");
+            break;
+        case 0:
+            printf("S");
+            break;
+        case -1:
+            printf("W");
+            break;
+        }
+        break;
+    case 'W':
+        switch (O) {
+        case 1:
+            printf("S");
+            break;
+        case 0:
+            printf("W");
+            break;
+        case -1:
+            printf("N");
+            break;
+        }
+    }
+
+    return 0;
+}
