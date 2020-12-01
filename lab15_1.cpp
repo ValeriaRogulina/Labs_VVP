3e47a0385333c0b0d88539a8d1288bef53f10a4b
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-12-01 17:14:49 +0700

    Add files via upload

---
+++ lab15_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+float PoweraA3(float A)
+{
+    float B;
+    B = A* A * A;
+    return (B);
+}
+
+int main()
+{
+    int i;
+    float a, b;
+    for (i = 1; i <= 5; i++) {
+
+        printf("A = ");
+        scanf_s("%f", &a);
+
+        b = PoweraA3(a);
+
+        printf("B = %f\n", b);
+    }
+    return 0;
+}
